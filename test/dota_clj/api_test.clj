57e1e27b5ff7912f936ac0ac201ea63b7aa5041d
Rohit Kumar <rohit.kumar@rutgers.edu>
2014-05-13 02:05:09 -0700

    Initial commit

---
+++ test/dota_clj/api_test.clj
@@ -0,0 +1,16 @@
+(ns dota-clj.api-test
+  (:use [clojure.test])
+  (:require [dota-clj :as api]
+            [environ.core :refer [env]]))
+
+(def api-key (env :steam-api-key))
+
+(deftest get-heroes-test
+  (let [hero-list (api/get-heroes :token api-key)]
+    ))
+
+(deftest get-match-history-test
+  (is false "TODO: Impl get-match-history test"))
+
+(deftest get-match-details-test
+  (is false "TODO: Impl get-match-details test"))
